b626cac0ef4d93f2a1854091faadc0c3615b7fb9
Joshika6124 <joshikareddy2k4@gmail.com>
2026-08-12 14:05:25 +0530

    Fix syntax error in get_app_debug_info function

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, make_response
 app = Flask(__name__)
 
 
-def get_app_debug_info():;
+def get_app_debug_info():
     cfg_items = {k: v for k, v in os.environ.items()}
     cfg_items['datetime'] = datetime.now().isoformat()
     return cfg_items
